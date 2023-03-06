76992fb9e6069cdb7e1457ac349efba5a3cd7dcf
Mateusz Szawczenko <m.szawczenko@interia.pl>
2023-03-06 20:25:44 +0100

    Configure static files, create base.html, base.css. Add bootstrap to home, collections and collection_details views.

--- swapi/urls.py
+++ swapi/urls.py
@@ -20,6 +20,6 @@ from datacollections import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_page, name="home"),
+    path('', views.home_page, name="home_page"),
     path('datacollections/', include('datacollections.urls'))
 ]
